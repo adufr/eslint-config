7d79e302f5d9d43efc7990c66b2c3d8789989c86
Arthur Dufour <dufourarthur.perso@gmail.com>
2025-02-23 17:15:49 +0100

    chore(eslint): simplify config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,21 +1,7 @@
 // import { adufr} from './dist/index.js'
 import { adufr } from './src/index'
 
-export default adufr(
-  [
-    {
-      files: ['src/**/*.ts'],
-      rules: {
-        'perfectionist/sort-objects': 'error',
-      },
-    },
-    {
-      files: ['**/*.md/*'],
-      rules: {
-        'perfectionist/sort-imports': 'off',
-        'perfectionist/sort-named-imports': 'off',
-      },
-    },
-  ],
-  { vue: true },
-)
+export default adufr([], {
+  prettier: true,
+  markdown: true,
+})
